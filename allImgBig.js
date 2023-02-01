51b80392763c3a58938d64ec2a7346d384a68c42
sword4869 <1084662708@qq.com>
2023-02-01 11:54:49 +0800

    4chan-src&style

--- allImgBig.js
+++ allImgBig.js
@@ -14,23 +14,18 @@
     let imgs = document.querySelector('.thread').querySelectorAll('img');
 
     // we want to replace link of small img to link of big img, but failed, because the width and height also need to accordingly change.
-    // for (let i = 0; i < imgs.length; i++) {
-    // for (let i = 0; i < 8; i++) {
-    //     let src_splits = imgs[i].src.split('.');
-    //     let big_src = "";
-    //     for (let j = 0 ; j < src_splits.length - 1; j++){
-    //         big_src += src_splits[j];
-    //         big_src += '.';
-    //     }
-    //     big_src = big_src.substring(0, big_src.length-2);
-    //     big_src += '.';
-    //     big_src += src_splits[src_splits.length - 1];
-    //     imgs[i].src = big_src;
-    //     console.log(big_src)
-    // }
-
     for (let i = 0; i < imgs.length; i++) {
-        imgs[i].click();
+        let src_splits = imgs[i].src.split('.');
+        let big_src = "";
+        for (let j = 0 ; j < src_splits.length - 1; j++){
+            big_src += src_splits[j];
+            big_src += '.';
+        }
+        big_src = big_src.substring(0, big_src.length-2);
+        big_src += '.';
+        big_src += src_splits[src_splits.length - 1];
+        imgs[i].src = big_src;
+        imgs[i].style = 'max-height: 100%; max-width: 100%';
     }
 
     // Your code here...
