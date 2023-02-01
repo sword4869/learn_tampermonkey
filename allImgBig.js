6dff2844494f2df698b860ed92c249c4ac574256
sword4869 <1084662708@qq.com>
2023-02-01 15:20:55 +0800

    4chan-parentElement.href

--- allImgBig.js
+++ allImgBig.js
@@ -15,16 +15,7 @@
 
     // we want to replace link of small img to link of big img, but failed, because the width and height also need to accordingly change.
     for (let i = 0; i < imgs.length; i++) {
-        let src_splits = imgs[i].src.split('.');
-        let big_src = "";
-        for (let j = 0 ; j < src_splits.length - 1; j++){
-            big_src += src_splits[j];
-            big_src += '.';
-        }
-        big_src = big_src.substring(0, big_src.length-2);
-        big_src += '.';
-        big_src += src_splits[src_splits.length - 1];
-        imgs[i].src = big_src;
+        imgs[i].src = imgs[i].parentElement.href
         imgs[i].style = 'max-height: 100%; max-width: 100%';
     }
 
